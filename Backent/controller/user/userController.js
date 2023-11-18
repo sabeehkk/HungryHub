be6849a377bc391c1cda75f7e0fd13ee4ffeeafc
sabeehkk <sabeehkadungalloor573@gmail.com>
2023-11-18 10:46:12 +0530

    removed unwanted codes

--- Backent/controller/user/userController.js
+++ Backent/controller/user/userController.js
@@ -1,16 +1,6 @@
 import userModel from '../../models/user.js'
 
 
-export const userProfile =async(req,res)=>{
-    try {
-         const result = await userModel.find()
-         console.log(result,'userdataaaaaa');
-    } catch (error) {
-      console.log(error.message);
-    }
-}
-
-
 export const updateProfile = async (req, res) => {
     try {
       console.log(req.body);
