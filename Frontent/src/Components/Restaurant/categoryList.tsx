7e8a158016f8fad2c5026776a169e30b0802564f
sabeehkk <sabeehkadungalloor573@gmail.com>
2023-11-26 21:06:02 +0530

    removed unwanted messages

--- Frontent/src/Components/Restaurant/categoryList.tsx
+++ Frontent/src/Components/Restaurant/categoryList.tsx
@@ -8,15 +8,14 @@ import CategoryModal from "../../Components/Restaurant/categoryModal";
 import { ErrorMessage, SuccessMessage } from "../../utils/util";
 
 const CategoryList = () => {
-
-  const [categories, setCategories] = useState([]) ;
-  const [is_deleted, setDeleted] = useState(false) ;
-  const [showModal, setShowModal] = useState(false) ;
-  const [editMode, setEditMode] = useState(false) ;
+  const [categories, setCategories] = useState([]);
+  const [is_deleted, setDeleted] = useState(false);
+  const [showModal, setShowModal] = useState(false);
+  const [editMode, setEditMode] = useState(false);
   const [categoryId, setCategoryId] = useState();
   const [categoryToEdit, setCategoryToEdit] = useState("");
   const [currentPage, setCurrentPage] = useState(1);
-  const itemsPerPage = 5 ;
+  const itemsPerPage = 5;
   const totalPages = Math.ceil(categories.length / itemsPerPage);
   const startIndex = (currentPage - 1) * itemsPerPage;
   const endIndex = startIndex + itemsPerPage;
@@ -75,28 +74,19 @@ const CategoryList = () => {
         .patch("/deleteCategory", { catId })
         .then((response) => {
           if (response.data.success) {
-            return SuccessMessage(response.data.message)
+            return SuccessMessage(response.data.message);
             console.log(response.data);
-            alert(response.data.message)
-            
-            // toast.success(response.data.message, {
-            //   position: toast.POSITION.TOP_CENTER,
-            //   autoClose: 1500,
-            // });
             setDeleted(!is_deleted);
           } else {
-           return ErrorMessage(response.data.message)
-          
+            return ErrorMessage(response.data.message);
           }
         })
         .catch((err) => {
-            // alert(err.message);
-          return ErrorMessage(err.data.message)
-            
-      
+          // alert(err.message);
+          return ErrorMessage(err.data.message);
         });
     } else {
-     return ErrorMessage('')
+      return ErrorMessage("");
     }
   };
 
