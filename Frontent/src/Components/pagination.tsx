23fbf52ee7bac5a008aa0a4d6cec052124b7cbf2
sabeehkk <sabeehkadungalloor573@gmail.com>
2023-11-26 23:12:48 +0530

    modified pagination edited

--- Frontent/src/Components/pagination.tsx
+++ Frontent/src/Components/pagination.tsx
@@ -4,53 +4,54 @@ const Pagination = ({ size, filterPagination, currentPage }) => {
   const limit = 3;
   return (
     <>
-  <div className="flex justify-center items-center py-4">
-  <ul className="pagination flex space-x-2">
-    <li>
-      <button
-        onClick={() => {
-          if (currentPage !== 1) {
-            filterPagination(currentPage - 1);
-          }
-        }}
-        className="hover:bg-gray-200 cursor-pointer p-2 rounded-full"
-        disabled={currentPage === 1}
-      >
-        &laquo; Prev
-      </button>
-    </li>
-    {[...Array(size)].map((_, i) => {
-      let pageNumber = i + 1;
+      <div className="flex justify-center items-center py-4">
+        <ul className="pagination flex space-x-2">
+          <li>
+            <button
+              onClick={() => {
+                if (currentPage !== 1) {
+                  filterPagination(currentPage - 1);
+                }
+              }}
+              className="hover:bg-gray-200 cursor-pointer p-2 rounded-full"
+              disabled={currentPage === 1}
+            >
+              &laquo; Prev
+            </button>
+          </li>
+          {[...Array(size)].map((_, i) => {
+            let pageNumber = i + 1;
 
-      return (
-        <li key={i}>
-          <button
-            className={`${
-              currentPage === pageNumber ? 'bg-blue-500 text-white' : 'bg-blue-200'
-            } hover:bg-blue-400 cursor-pointer p-2 rounded-full`}
-            onClick={() => filterPagination(pageNumber)}
-          >
-            {pageNumber}
-          </button>
-        </li>
-      );
-    })}
-    <li>
-      <button
-        onClick={() => {
-          if (currentPage !== size) {
-            filterPagination(currentPage + 1);
-          }
-        }}
-        className="hover:bg-gray-200 cursor-pointer p-2 rounded-full"
-        disabled={currentPage === size}
-      >
-        Next &raquo;
-      </button>
-    </li>
-  </ul>
-</div>
-
+            return (
+              <li key={i}>
+                <button
+                  className={`${
+                    currentPage === pageNumber
+                      ? "bg-blue-500 text-white"
+                      : "bg-blue-200"
+                  } hover:bg-blue-400 cursor-pointer p-2 rounded-full`}
+                  onClick={() => filterPagination(pageNumber)}
+                >
+                  {pageNumber}
+                </button>
+              </li>
+            );
+          })}
+          <li>
+            <button
+              onClick={() => {
+                if (currentPage !== size) {
+                  filterPagination(currentPage + 1);
+                }
+              }}
+              className="hover:bg-gray-200 cursor-pointer p-2 rounded-full"
+              disabled={currentPage === size}
+            >
+              Next &raquo;
+            </button>
+          </li>
+        </ul>
+      </div>
     </>
   );
 };
