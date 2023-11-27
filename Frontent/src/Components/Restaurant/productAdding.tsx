83bb1bd7b9c68c925e152f2e0943b2096ef0bcb5
sabeehkk <sabeehkadungalloor573@gmail.com>
2023-11-27 20:12:33 +0530

    product adding design progression

--- Frontent/src/Components/Restaurant/productAdding.tsx
+++ Frontent/src/Components/Restaurant/productAdding.tsx
@@ -19,7 +19,6 @@ const AddProduct: React.FC = () => {
   const navigate = useNavigate();
 
   const restaurant = useSelector((state) => state.restaurentAuth);
- 
 
   let result = restaurant.restaurent;
   console.log(result, "restuultt");
@@ -77,7 +76,6 @@ const AddProduct: React.FC = () => {
   }, [selectedImage]);
 
   const addProduct = async () => {
-    
     if (
       productName.trim() === "" ||
       description.trim() === "" ||
@@ -91,7 +89,7 @@ const AddProduct: React.FC = () => {
     }
     const urlImages = await handleImageUpload(images);
     console.log("image url ", urlImages);
-   
+
     if (productName.trim().length === 0 || description.trim().length === 0) {
       setErrors(true);
     } else {
@@ -103,7 +101,7 @@ const AddProduct: React.FC = () => {
         category,
         images: urlImages,
         restId,
-      } ;
+      };
       restaurentAxios
         .post("/addProduct", FormData)
         .then((response) => {
@@ -118,120 +116,128 @@ const AddProduct: React.FC = () => {
     }
   };
 
-
   return (
-    <div className="p-10">
-      <div className="md:flex p-4">
-        <div className="md:w-1/2 leading-6">
-          <label className="block font-medium">Product Name:</label>
-          {!productName.trim().length && errors && (
-            <p className="text-red-500 text-sm">{"Product Name is required"}</p>
-          )}
-          <input
-            type="text"
-            id="name"
-            name="name"
-            value={productName}
-            onChange={(e) => {
-              setProductName(e.target.value);
-            }}
-            required
-            className="border border-gray-300 rounded-sm md:w-3/5 bg-gray-300 mb-5 py-1 w-full"
-          />
-          <label htmlFor="category" className="block font-medium">
-            Category:
-          </label>
-          <select
-            id="category"
-            name="category"
-            value={category}
-            onChange={(e) => setCategory(e.target.value)}
-            required
-            className="border border-gray-300 rounded-sm md:w-3/5 bg-gray-300 mb-5 py-1 w-full"
-          >
-            <option value="">Select a category</option>
-            {categories.map((category) => (
-              <option key={category._id} value={category._id}>
-                {category.name}
-              </option>
-            ))}
-          </select>
-
-          <label htmlFor="description" className="block font-medium">
-            Description:
-          </label>
-          {!description.trim().length && errors && (
-            <p className="text-red-500 text-sm">{"Description is required"}</p>
-          )}
-          <input
-            type="text"
-            id="description"
-            name="description"
-            value={description}
-            onChange={(e) => {
-              setDescription(e.target.value);
-            }}
-            required
-            className="border border-gray-300 rounded-sm md:w-3/5 bg-gray-300 mb-5 h-32 w-full"
-          />
-          {!category.trim().length && errors && (
-            <p className="text-red-500 text-sm">{"Select a Category"}</p>
-          )}
-
-          <label htmlFor="price" className="block font-medium">
-            Price:
-          </label>
-          <input
-            type="number"
-            id="price"
-            name="price"
-            value={productPrice}
-            onChange={(e) => {
-              setProductPrice(e.target.value);
-            }}
-            required
-            className="border border-gray-300 rounded-sm md:w-3/5 bg-gray-300 mb-5 py-1 w-full"
-          />
-        </div>
-        <div className="md:w-1/2">
-          <label htmlFor="restId" className="block font-medium">
-            Restaurant ID
-          </label>
-          <input
-            type="text"
-            id="restId"
-            value={restId}
-            readOnly
-            className="border border-gray-300 rounded-sm md:w-3/5 bg-gray-300 mb-5 py-1 w-full"
-          />
-
-          <div className="custom-file mt-3 h-52 items-center justify-center bg-gray-300 md:w-3/5 w-full">
-            <label htmlFor="profImage" className=""></label>
+    <>
+      <div className="text-center ">
+        <h2 className="text-4xl font-extrabold">{"Add Your Food Details"}</h2>
+      </div>
+      <div className="p-10">
+        <div className="md:flex p-4">
+          <div className="md:w-1/2 leading-6">
+            <label className="block font-medium">Product Name:</label>
+            {!productName.trim().length && errors && (
+              <p className="text-red-500 text-sm">
+                {"Product Name is required"}
+              </p>
+            )}
             <input
-              className="form-control custom-file-input"
-              name="file"
-              multiple
-              type="file"
-              id="fileInput"
+              type="text"
+              id="name"
+              name="name"
+              value={productName}
+              onChange={(e) => {
+                setProductName(e.target.value);
+              }}
               required
-              onChange={handleImages}
-              min={4}
-              max={5}
+              className="border border-gray-300 rounded-sm md:w-3/5 bg-gray-300 mb-5 py-1 w-full"
+            />
+            <label htmlFor="category" className="block font-medium">
+              Category:
+            </label>
+            <select
+              id="category"
+              name="category"
+              value={category}
+              onChange={(e) => setCategory(e.target.value)}
+              required
+              className="border border-gray-300 rounded-sm md:w-3/5 bg-gray-300 mb-5 py-1 w-full"
+            >
+              <option value="">Select a category</option>
+              {categories.map((category) => (
+                <option key={category._id} value={category._id}>
+                  {category.name}
+                </option>
+              ))}
+            </select>
+
+            <label htmlFor="description" className="block font-medium">
+              Description:
+            </label>
+            {!description.trim().length && errors && (
+              <p className="text-red-500 text-sm">
+                {"Description is required"}
+              </p>
+            )}
+            <input
+              type="text"
+              id="description"
+              name="description"
+              value={description}
+              onChange={(e) => {
+                setDescription(e.target.value);
+              }}
+              required
+              className="border border-gray-300 rounded-sm md:w-3/5 bg-gray-300 mb-5 h-32 w-full"
+            />
+            {!category.trim().length && errors && (
+              <p className="text-red-500 text-sm">{"Select a Category"}</p>
+            )}
+
+            <label htmlFor="price" className="block font-medium">
+              Price:
+            </label>
+            <input
+              type="number"
+              id="price"
+              name="price"
+              value={productPrice}
+              onChange={(e) => {
+                setProductPrice(e.target.value);
+              }}
+              required
+              className="border border-gray-300 rounded-sm md:w-3/5 bg-gray-300 mb-5 py-1 w-full"
             />
           </div>
-          <div className="pt-10">
-            <button
-              className="bg-red-500 text-white active:bg-red-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
-              type="button"
-              onClick={addProduct}
-            >
-              Add Product
-            </button>
+          <div className="md:w-1/2">
+            <label htmlFor="restId" className="block font-medium">
+              Restaurant ID
+            </label>
+            <input
+              type="text"
+              id="restId"
+              value={restId}
+              readOnly
+              className="border border-gray-300 rounded-sm md:w-3/5 bg-gray-300 mb-5 py-1 w-full"
+            />
+
+            <div className="custom-file mt-3 h-52 items-center justify-center bg-gray-300 md:w-3/5 w-full">
+              <label htmlFor="profImage" className=""></label>
+              <input
+                className="form-control custom-file-input"
+                name="file"
+                multiple
+                type="file"
+                id="fileInput"
+                required
+                onChange={handleImages}
+                min={4}
+                max={5}
+              />
+            </div>
+            <div className="pt-10">
+              <button
+                className="bg-red-500 text-white active:bg-red-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                type="button"
+                onClick={addProduct}
+              >
+                Add Product
+              </button>
+            </div>
+            <div className="pt-3 "></div>
           </div>
-          <div className="pt-3 "></div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
