f479bfcb60a6127f84fa9cc13abcc49095a0933c
sabeehkk <sabeehkadungalloor573@gmail.com>
2023-11-27 20:17:46 +0530

    style progress

--- Frontent/src/Components/Restaurant/productAdding.tsx
+++ Frontent/src/Components/Restaurant/productAdding.tsx
@@ -228,7 +228,7 @@ const AddProduct: React.FC = () => {
             </div>
             <div className="pt-10">
               <button
-                className="bg-red-500 text-white active:bg-red-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                className="ml-14 bg-teal-500 text-white active:bg-red-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
                 type="button"
                 onClick={addProduct}
               >
