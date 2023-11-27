4692ef2e8153ca7bef3e7f0d5c8a2c94f68af8ff
sabeehkk <sabeehkadungalloor573@gmail.com>
2023-11-27 18:30:39 +0530

    fixed navbar as sticky

--- Frontent/src/Components/User/userNavbar.tsx
+++ Frontent/src/Components/User/userNavbar.tsx
@@ -11,7 +11,7 @@ const UserNavbar = () => {
   return (
     <div
       className="max-w-[1640px] mx-auto
-     flex justify-between items-center  p-4"
+     flex justify-between items-center  p-4 sticky top-0 bg-white z-50"
     >
       {/* {Left Side} */}
       <div className="flex items-center ">
