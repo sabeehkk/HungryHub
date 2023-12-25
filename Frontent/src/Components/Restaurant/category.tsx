c8ee2b3294320b1d3918aa1cb0ee465ef28169ae
sabeehkk <sabeehkadungalloor573@gmail.com>
2023-12-25 15:07:51 +0530

    cleanup in category page

--- Frontent/src/Components/Restaurant/category.tsx
+++ Frontent/src/Components/Restaurant/category.tsx
@@ -1,17 +1,11 @@
 import React, { useState, useEffect } from "react";
-import { useNavigate } from "react-router-dom";
 import { useSelector } from "react-redux";
 import { restaurentAxios } from "../../axios/axios";
 
-const CategoryModal = ({ showModal }) => {
+const CategoryModal = ({ showModal}) => {
   const [categoryName, setCategoryName] = useState("");
-
   const restaurent = useSelector((state) => state.restaurentAuth);
-  console.log(restaurent, "restaurent details in category page");
-
   const restId = restaurent.restaurent._id;
-  console.log(restId);
-
   const handleAddCategory = () => {
     restaurentAxios
       .post("/addCategory", { categoryName, restId })
@@ -31,7 +25,6 @@ const CategoryModal = ({ showModal }) => {
         }}
         className="w-full p-2 border border-gray-300"
       />
-
       <button
         onClick={handleAddCategory}
         className="bg-green-600 text-off-White mt-2 p-1 rounded-sm"
