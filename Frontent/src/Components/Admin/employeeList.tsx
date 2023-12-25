2d068f80ae1c6c69de52cbb30eac54592591fdc1
sabeehkk <sabeehkadungalloor573@gmail.com>
2023-12-25 14:49:15 +0530

    cleanup in restaurent list

--- Frontent/src/Components/Admin/employeeList.tsx
+++ Frontent/src/Components/Admin/employeeList.tsx
@@ -10,20 +10,16 @@ function EmployeeListTable() {
   const dispatch = useDispatch();
   const [page, setPage] = useState(1);
   const [size, setSize] = useState(1);
-
   useEffect(() => {
     getEmployeeData();
   }, [update, page]);
-
   const getEmployeeData = async () => {
     const { employeeData, size } = await employeesData(page);
-
     setEmployeeList(employeeData);
     setSize(size);
     setUpdate("");
     dispatch(addEmployees(employeeData));
   };
-
   const handleAction = async (id: string, action: string, message: string) => {
     await employeesActionAPI(id, action);
     setUpdate("update");
@@ -44,5 +40,4 @@ function EmployeeListTable() {
     />
   );
 }
-
 export default EmployeeListTable;
